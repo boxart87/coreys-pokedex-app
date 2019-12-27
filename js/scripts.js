cf1144dcb03e666dbfa5ff012574ef883f8d5f34
Corey Weed <boxart1987@gmail.com>
2019-12-26 23:10:56 -0800

    started my repository to store data of the pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-var favoriteFood = 'My favorite food is Pizza.';
-document.write(favoriteFood);
+var repository = [
+    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'], abilities: ['chlorophyll', 'overgrow'] },
+    { name: 'Charmander', height: 8, types: 'fire', abilities: ['blaze', 'solar-power'] },
+    { name: 'Squirtle', height: 8, types: 'water', abilities: ['rain-dish', 'torrent'] },
+];

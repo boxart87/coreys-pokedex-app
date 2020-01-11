cabb75ab8b3325f4cef5906d4eba2c2bc586d343
Corey Weed <boxart1987@gmail.com>
2020-01-10 23:54:17 -0800

    wrapped the repository with an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-var repository = [
-    { name: 'Bulbasaur', height: 0.7, types: [' grass', ' poison'], abilities: [' chlorophyll', ' overgrow'] },
-    { name: 'Charmeleon', height: 1.1, types: ' fire', abilities: [' blaze', ' solar-power'] },
-    { name: 'Squirtle', height: 0.5, types: ' water', abilities: [' rain-dish', ' torrent'] },
-];
+var pokemonRepository = (function () {
+    var repository = [
+        { name: 'Bulbasaur', height: 0.7, types: [' grass', ' poison'], abilities: [' chlorophyll', ' overgrow'] },
+        { name: 'Charmeleon', height: 1.1, types: ' fire', abilities: [' blaze', ' solar-power'] },
+        { name: 'Squirtle', height: 0.5, types: ' water', abilities: [' rain-dish', ' torrent'] },
+    ];
 
-repository.forEach(function (currentItem) {
+    function add(pokemon) {
+        repository.push(pokemon);
+    }
+
+    function getAll() {
+        return repository;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+
+pokemonRepository.getAll().forEach(function (currentItem) {
     document.write('<h1>' + currentItem.name + '</h1>' + '<p><strong>Height:</strong> ' + currentItem.height + 'm');
     if (currentItem.height > 1) {
         document.write(' - Wow, that\'s big! </p>')
